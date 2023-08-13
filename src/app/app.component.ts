6d3e0ae61747c35b4e76fc716a569ab117874e34
Jeffrey Albertson <me@jeffreyalbertson.info>
2023-08-12 18:23:45 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,12 +63,12 @@ export class AppComponent implements OnInit, OnDestroy {
   logout() {
     if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
       this.authService.logoutPopup({
-        postLogoutRedirectUri: "/",
+        postLogoutRedirectUri: "/.auth/login/aad/callback",
         mainWindowRedirectUri: "/"
       });
     } else {
       this.authService.logoutRedirect({
-        postLogoutRedirectUri: "/",
+        postLogoutRedirectUri: "/.auth/login/aad/callback",
       });
     }
   }
